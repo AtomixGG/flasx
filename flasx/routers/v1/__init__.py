543d7e8e45510ddcc2c5f6892f2775a24ceff61f
6510110632 <nydukkung@gmail.com>
2025-07-07 10:12:59 +0700

    feat: implement routers for customer, delivery staff, parcel, station, and vehicle; remove item and receiver routers

--- flasx/routers/v1/__init__.py
+++ flasx/routers/v1/__init__.py
@@ -1,6 +1,15 @@
 from fastapi import APIRouter
-from . import item_router, receiver_router
+from . import (
+    customer_router,
+    station_router,
+    vehicle_router,
+    delivery_staff_router,
+    parcel_router,
+)
 
 router = APIRouter(prefix="/v1")
-router.include_router(item_router.router)
-router.include_router(receiver_router.router)
+router.include_router(customer_router.router)
+router.include_router(station_router.router)
+router.include_router(vehicle_router.router)
+router.include_router(delivery_staff_router.router)
+router.include_router(parcel_router.router)
